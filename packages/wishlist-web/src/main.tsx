f7c9511cb2ade883bcc9da94a8b28a9005a4a1f3
RESPRiT <10722616+RESPRiT@users.noreply.github.com>
2025-11-10 12:40:00 -0600

    initial setup

--- packages/wishlist-web/src/main.tsx
+++ packages/wishlist-web/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "@/styles/globals.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -8,12 +7,26 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { ThemeProvider } from "./contexts/ThemeContext.tsx";
 
+import { routeTree } from "./routeTree.gen.ts";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+
+// via: https://tanstack.com/router/latest/docs/framework/react/installation/manual
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 library.add(fas, far);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>,
 );
