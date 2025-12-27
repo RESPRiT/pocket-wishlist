1af60aa7a9401207172070433b9cce9d0eef500c
RESPRiT <10722616+RESPRiT@users.noreply.github.com>
2025-12-27 14:47:11 -0600

    Updates

--- server/assets/_userId-DQfGNYoZ.js
+++
@@ -1,7 +0,0 @@
-import { jsx } from "react/jsx-runtime";
-function RouteComponent() {
-  return /* @__PURE__ */ jsx("div", { children: 'Hello "/$userId"!' });
-}
-export {
-  RouteComponent as component
-};
