317cea865b408f9ef9ee8b60501b92203f8796f9
RESPRiT <10722616+RESPRiT@users.noreply.github.com>
2026-01-20 15:03:02 -0600

    Update pricegun schema for latest API changes

--- packages/wishlist-shared/schemas/api.ts
+++ packages/wishlist-shared/schemas/api.ts
@@ -3,7 +3,10 @@ import { z } from "zod";
 // TODO: Don't copy to wishlist-api
 export const WishlistSchema = z.record(
   z.coerce.number<number>(), // JSON keys are strings
-  z.literal(["NONE", "PACKAGED", "OPENED", "WISHED"])
+  z.preprocess(
+    (val) => val ?? "NONE",
+    z.literal(["NONE", "PACKAGED", "OPENED", "WISHED"]),
+  ),
 );
 export const WishlistResponseSchema = z.object({
   username: z.string(),
@@ -18,34 +21,36 @@ export const WishlistToggleRequestSchema = z.object({
     z.object({
       id: z.number(),
       status: z.boolean(),
-    })
+    }),
   ),
 });
 export type Wishlist = z.infer<typeof WishlistSchema>;
 export type WishlistResponse = z.infer<typeof WishlistResponseSchema>;
 export type WishlishToggleRequest = z.infer<typeof WishlistToggleRequestSchema>;
 
+// NOTE: All pricegun number values are coerced from strings bc all the bigint
+//       values are converted into strings by the API
 // most recent sales data
 export const PriceGunSalesDataSchema = z.object({
   date: z.coerce.date(),
-  unitPrice: z.number(),
-  quantity: z.number(),
+  unitPrice: z.coerce.number(),
+  quantity: z.coerce.number(),
 });
 // data for a specific date
 export const PriceGunHistoricalDataSchema = z.object({
-  itemId: z.number(),
+  itemId: z.coerce.number(),
   date: z.coerce.date(),
-  volume: z.number(),
-  price: z.coerce.number(), // TODO: this coercion is to get around a type bug with pricegun
+  volume: z.coerce.number(),
+  price: z.coerce.number(),
 });
 export const PriceGunSchema = z.object({
   // value across ALL transactions, not just past 2 weeks
-  value: z.number(),
+  value: z.coerce.number(),
   // volume across the past 2 weeks
-  volume: z.number(),
+  volume: z.coerce.number(),
   // last time the price value was calculated by PriceGun
   date: z.coerce.date(), // JSON dates are strings
-  itemId: z.number(),
+  itemId: z.coerce.number(),
   name: z.string().nullable(), // new IOTMs can have a missing name field, I guess?
   image: z.string(),
   sales: z.array(PriceGunSalesDataSchema),
@@ -55,7 +60,6 @@ export const PriceGunResponseSchema = z.array(PriceGunSchema);
 export type PriceGun = z.infer<typeof PriceGunSchema>;
 export type PriceGunResponse = z.infer<typeof PriceGunResponseSchema>;
 
-// TODO: Maybe rename MallPriceResponseSchema
 export const MallPriceSchema = z.record(z.coerce.number<number>(), z.number());
 export const PriceSchema = z.object({
   // from lowest mall
@@ -66,7 +70,7 @@ export const PriceSchema = z.object({
 });
 export const CombinedPriceSchema = z.record(
   z.coerce.number<number>(),
-  PriceSchema
+  PriceSchema,
 );
 export const MallPriceResponseSchema = z.object({
   prices: CombinedPriceSchema,
